c3987d4240ce26b92014de2a57ac29844ed74d09
neteliy <760442081@qq.com>
2021-10-14 16:14:04 +0800

    Signed-off-by: neteliy <760442081@qq.com>

--- areas/tab/tab_controller.js
+++ areas/tab/tab_controller.js
@@ -8,13 +8,6 @@ angular.module('tab.controller', ['tab.service'])
       count:""
     }
     $scope.$on('$ionicView.beforeEnter', function (e) {
-    var promise = tabFty.getAllData();
-        promise.then(
-      function (data) {
-              // $scope.obj_cartCount.count="0";
-              // for(var i =0;i<data.length;i++){
-              //   $scope.obj_cartCount.count=parseInt($scope.obj_cartCount.count)+parseInt(data[i].number);
-              // }
-      });
+
     });
   });
